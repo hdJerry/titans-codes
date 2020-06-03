950619e1c22d7b5ad3cee69587f9eec49d205d61
hdjerry <damilolajerryhd@gmail.com>
2020-06-03 05:24:03 +0100

    titanhs

--- scripts/test.js
+++ scripts/test.js
@@ -1,18 +1,9 @@
 
-  let details = {
-    fname: "Jerry Hogan",
-    id: "HNG-01701",
-    language: "JavaScript"
-  }
 
   const intro = (datas) =>{
-        let { fname, id, language } = datas;
+        
 
-        let introduction = `Hello World, this is [${fname}] with HNGi7 ID [${id}] using [${language}] for stage 2 task`;
-        // console.log('\n');
-        // console.log(introduction);
-        // console.log('\n');
-
+        let introduction = `Hello World, this is [Jerry Hogan] with HNGi7 ID [HNG-01701] using [JavaScript] for stage 2 task`;
         return introduction;
     }
 
